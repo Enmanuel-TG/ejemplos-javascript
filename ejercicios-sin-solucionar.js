63ef01e6cfb101c103fbfb3433188167433212ad
Adonys <58638286+adonyssantos@users.noreply.github.com>
2023-04-22 20:06:23 +0000

    Agrega ejercicios

---
+++ ejercicios-sin-solucionar.js
@@ -0,0 +1,50 @@
+// RESUELVE LOS SIGUIENTE EJERCICIOS, PUEDES BUSCAR EN GOOGLE LAS FORMULAS O INCLUSO PREGUNTARLE A CHAT GPT POR LA FORMULA, PERO NO LA RESOLUCIÓN DEL CÓDIGO.
+
+// Fibonacci: Escribe una función que calcule los primeros n números de la secuencia de Fibonacci.
+
+function fibonacci(n) {
+  // Código...
+}
+
+// Suma de números: Escribe una función que tome un arreglo de números y devuelva la suma de todos los números de dicho arreglo.
+
+function sumArray(numbersArray) {
+  // Código...
+}
+
+// Factorial: Escribe una función que calcule el factorial de un número dado. El factorial son la suma n números desde 1 hasta dicho, por ejemplo el factorial de 5 es 1 + 2 + 3 + 4 + 5, es decir 15.
+function factorial(n) {
+  // Código...
+}
+
+// Números primos: Escribe una función que determine si un número dado es primo o no.
+
+// Ordenamiento de lista: Escribe una función que ordene un arreglo de números de menor a mayor.
+
+function ordenar(numberArray) {
+  // Código...
+}
+
+// Palíndromos: Escribe una función que determine si una palabra dada es un palíndromo o no.
+
+function esPalindromo(palabra) {
+  // Código
+}
+
+// Conversión de Celsius a Fahrenheit: Escribe una función que convierta grados Celsius a grados Fahrenheit.
+
+function convertir(temperatura) {
+  // Código...
+}
+
+// Generador de contraseñas: Escribe una función que genere una contraseña aleatoria. Es decir, que cada vez que ejecute la función, esta devuelva un string con números, letras y simbolos aleatorios. Ejemplos: "f&*fj29Dgr", "b4B4n*3n@", "afJ*#fF#3f", "fH&9*#hfa3L8!"
+
+function generatePassword() {
+  // Código...
+}
+
+// Cálculo de área de un triángulo: Escribe una función que calcule el área de un triángulo.
+
+function calcularArea(base, altura) {
+  // Código...
+}
